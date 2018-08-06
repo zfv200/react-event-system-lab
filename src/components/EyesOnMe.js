231b986aeb60d4818e02009b16133ad3734e9bc5
zfv200 <zach.vary@flatironschool.com>
2018-08-06 16:52:10 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component{
+
+  onFocusEvent = () => {
+    console.log('Good!');
+  }
+
+  onBlurEvent = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  render(){
+    return (
+      <div>
+        <button
+          onFocus={this.onFocusEvent}
+          onBlur={this.onBlurEvent}
+        ></button>
+      </div>
+    )
+  }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,34 @@
-// Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component{
+
+  constructor(){
+    super()
+    this.state={
+      value: ""
+    }
+  }
+
+  renderKeys = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  logKeys = () => {
+    console.log('Entering password...');
+  }
+
+  render(){
+    return(
+      <div>
+        <input
+        onKeyUp={this.logKeys}
+        onChange={this.renderKeys}
+        type="password"/>
+      </div>
+    )
+  }
+}
+
+export default Keypad
